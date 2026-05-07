8e618240017bc044cf91fd391d5c1f8b2a7cdb22
RoL4iks <Rol4ik10@gmail.com>
2026-05-07 21:13:09 +0300

    added some js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 const button = document.getElementById("downloadBtn");
 
 button.addEventListener("click", () => {
-    alert("You can't download videos right now!");
-    window.location.href = 'beta.html';
+    console.warn("user tried to push the download button");
+    if(confirm("The button isn't working, you want to read why?")) {
+        window.location.href = 'beta.html'
+    }
+    
+    
 });
+
+const file_type = document.getElementById("selectbtn")
+
+file_type.addEventListener("click", () => {
+    alert("File choosing is not availiable!");
+    console.warn("File type isn't working")
+})
